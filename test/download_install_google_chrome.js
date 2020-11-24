d7aa99bc6fa0a1ecb53e3b5e9bb3abdeb916e81b
Marcus Cruz <marcusfunctional01@gmail.com>
2020-11-24 00:05:20 +0000

    first commit

--- test/download_install_google_chrome.js
+++ test/download_install_google_chrome.js
@@ -3,6 +3,7 @@
 const { expect, assert, should } = require('chai');
 const { elementIsVisible } = require('selenium-webdriver/lib/until');
 
+
 var webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
@@ -14,7 +15,7 @@ var url = "https://www.google.com/chrome/";
 describe("Filter the computer list", () => {
 
     after(() => {
-        //driver.quit();
+        driver.quit();
     });
 
     beforeEach(() => {
@@ -26,7 +27,7 @@ describe("Filter the computer list", () => {
     });
     
     afterEach(() => {   
-       // driver.quit();
+       driver.quit();
     });
 
 
@@ -37,39 +38,14 @@ describe("Filter the computer list", () => {
             .then(() => driver.findElement(By.className('chr-homepage-hero__download chr-grid-child')).click())
             .then((DownloadChrome => assert.strictEqual(DownloadChrome), "Download Chrome"))
             .then(() => driver.findElement(By.id("js-accept-install--linux")).click())
-            .then((AcceptAndInstall) => assert.strictEqual("Accept and Install"))
-            //.then(() => driver.findElement(By.))
-            
-            //assert.isString('chr-homepage-hero__download chr-grid-child')
-            //.then(() => driver.findElement('js-download-hero')).click())
-            // .then(() =>  driver.findElement(By.name('f')).sendKeys('ARRA'))
-            //.then(() =>  driver.findElement(By.className(''))
-            // .then(() =>  driver.findElement(By.name('f')).getAttribute('value'))
-            // .then(filterFieldDisplayedAfter => assert.equal(filterFieldDisplayedAfter, "ARRA"))
-
-            // .then(() => driver.quit())
-            // .then(done)
+            .then((AcceptAndInstall) => assert.strictEqual(AcceptAndInstall), "Accept and Install")
+            .then((CookiesButton => assert.strictEqual("Ok, got it")))
+            .then(() => driver.findElement(By.id('thank-you-copy-container')))
+            .then((ThankYouForDownloading => assert.strictEqual(ThankYouForDownloading), 'Thank you for downloading Chrome!'))
             .catch(err => done(err));
 
    
     });
 
-    // it("He sees empty list of computers when filter doesn't match any existing name", done => {
-    //     driver.get(url)
-    //         .then(() =>  driver.getTitle())
-    //         .then(pageTitle => assert.equal(pageTitle, "Computers database"))
-    //         .then(() =>  driver.findElement(By.name('f')).sendKeys('Idonotexist'))
-    //         .then(() =>  driver.findElement(By.id('searchsubmit')).click())
-    //         .then(() =>  driver.findElement(By.name('f')).getAttribute('value'))
-    //         .then(filterFieldDisplayedAfter => assert.equal(filterFieldDisplayedAfter, "Idonotexist"))
-    //         .then(() =>  driver.findElement(By.xpath('//*[@id="main"]/h1')).getText())
-    //         .then(noComputersTitle => assert.equal(noComputersTitle, "No computers found"))
-    //         .then(() =>  driver.findElement(By.xpath('//*[@id="main"]/div[2]/em')).getText())
-    //         .then(noneComputersMessage => assert.equal(noneComputersMessage, "Nothing to display"))
-    //         .then(() => driver.quit())
-    //         .then(done)
-    //         .catch(err => done(err));
-    // });
-
-
 });
+
